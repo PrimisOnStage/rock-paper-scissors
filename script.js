cb4381a37c14f22c4a2cb10a0e4e37b26887bfc9
PrimisOnStage <pratham.limitless@gmail.com>
2026-05-26 09:47:00 +0530

    Add simple UI with buttons for each player move
    Player selects a button from Rock Paper and Scissor to play a round

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ function numToObject(num){
     return (num === 0 ? "rock" : num===1 ? "paper" : "scissors");
 }
 
+function playRound(player_select){
+    let comp_select = Math.floor(Math.random()*3);
+    comp_select = numToObject(comp_select);
 
-for (let i = 0; i<5; i++){
-    let comp_select = Math.floor(Math.random()*3)
-
-    let player_select = parseInt(prompt("Select: Rock (0), Paper(1) or Scissors(2)"));
-
-    console.log(`Player chose ${numToObject(player_select)} and Computer Chose ${numToObject(comp_select)}\n`);
+    console.log(`Player chose ${player_select} and Computer Chose ${comp_select}\n`);
     if (comp_select === player_select){
         console.log("draw");
     }
-    else if((comp_select == 0 && player_select == 1) || (comp_select == 1 && player_select == 2) || comp_select==2 && player_select==0){
-        console.log("Player Wins!")
+    else if((comp_select == "rock" && player_select == "paper") || (comp_select == "paper" && player_select == "scissors") || comp_select=="scissors" && player_select=="rock"){
+        console.log("Player Wins!");
         player_score++;
     }
     else{
-        console.log("Computer Wins!")
+        console.log("Computer Wins!");
         comp_score++;
     }
-    console.log(`Player Score is ${player_score} and Computer Score is ${comp_score}`)
+    console.log(`Player Score is ${player_score} and Computer Score is ${comp_score}`);
 }
 
-console.log(player_score>comp_score?"player wins":"computer wins");
+const buttons = document.querySelectorAll("#playerSelection button");
+console.log(buttons);
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        playRound(button.className);
+
+    });
+});
+
+
